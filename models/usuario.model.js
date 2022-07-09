21bc972f890230e2d9963d8acbdd54ec380fb50a
J1Loop <jorge.ua.1996@gmail.com>
2022-07-09 17:54:02 +0200

    Método create hecho en usuario model, POST para el register en api/usuarios.js hecho también

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -1,5 +1,9 @@
-const { executeQuery, executeQueryOne } = require( '../helpers/utils' )
+const { executeQuery, executeQueryOne } = require('../helpers/utils')
 
+const create = ({ email, password, nombre_completo, direccion }) => {
+    return executeQuery(
+        'INSERT INTO usuario (email, password, nombre_completo, direccion) VALUES (?,?,?,?)',
+        [email, password, nombre_completo, direccion]);
+};
 
-
-module.exports = {}
+module.exports = { create }
--- routes/api/usuarios.js
+++ routes/api/usuarios.js
@@ -1,9 +1,44 @@
-const router = require('express').Router()
+const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => {
-	res.json({ success: 'success' })
-})
+const Usuario = require('../../models/usuario.model');
+
+router.post('/registro',
+	body('email')
+		.exists()
+		.withMessage('El campo de email es requerido')
+		.isEmail()
+		.withMessage('El email debe tener un formato correcto'),
+	body('password')
+		.exists()
+		.withMessage('El campo de password es requerido'),
+	body('nombre_completo')
+		.exists()
+		.withMessage('El campo de nombre_completo es requerido'),
+	body('direccion')
+		.exists()
+		.withMessage('El campo de direccion es requerido')
+	, async (req, res) => {
+
+		// Comprobar los errores de las diferentes validaciones
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.json(errors.array());
+		};
+
+		try {
+			// Encriptasió de pass
+			req.body.password = bcrypt.hashSync(req.body.password, 13);
+
+			// Creación del usuario
+			const result = await Usuario.create(req.body);
+			res.json(result);
+		} catch (err) {
+			res.json({ error: err.message });
+		}
+
+	});
 
 
-module.exports = router
+module.exports = router;

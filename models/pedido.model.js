0fda0a506b6b3393262532a04d9f86f7472e0993
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-07-09 19:31:21 +0200

    Cambio de nombre del metodo a uno mas descriptivo

--- models/pedido.model.js
+++ models/pedido.model.js
@@ -1,7 +1,15 @@
 const { executeQuery, executeQueryOne } = require( '../helpers/utils' )
+const Producto = require( './producto.model' )
 
-const getById = ( id ) => {
-	return executeQuery( 'select * from pedidos where usuario_id = ?', [ id ] )
+const getAllProductsById = async ( id ) => {
+	let pedidos = await executeQuery( 'select * from pedidos where usuario_id = ?', [ id ] )
+	let pedidosReturn = []
+	for ( let pedido of pedidos ) {
+		pedidosReturn.push( await Producto.getProductById( pedido.producto_id ) )
+		console.log( pedidosReturn )
+	};
+	console.log( pedidos )
+	return pedidosReturn
 }
 
-module.exports = { getById }
+module.exports = { getAllProductsById }

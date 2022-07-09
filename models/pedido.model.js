7913d6356b5f4b1fa6b14d1c7a1dc7cda699a93c
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-07-09 20:39:39 +0200

    Cambio de array por JSON

--- models/pedido.model.js
+++ models/pedido.model.js
@@ -4,8 +4,8 @@ const Producto = require( './producto.model' )
 const getAllProductsById = async ( id ) => {
 	let pedidos = await executeQuery( 'select * from pedidos where usuario_id = ?', [ id ] )
 	for ( let pedido of pedidos ) {
-		pedido.producto = []
-		pedido.producto.push( await Producto.getProductById( pedido.producto_id ) )
+		pedido.producto = {}
+		pedido.producto = ( await Producto.getProductById( pedido.producto_id ) )
 	};
 	return pedidos
 }

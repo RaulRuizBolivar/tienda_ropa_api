fad8debebcf12a776abe7e0f06651b0f71627836
Alejandro Fernández Martín <ale.fernandez612@gmail.com>
2022-07-10 10:28:17 +0200

    Cambios aceptados en api.js

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,10 @@
-const router = require( 'express' ).Router();
+const router = require('express').Router();
 
-const apiProductosRouter = require( './api/productos' );
-const apiUsuariosRouter = require( './api/usuarios' );
+const apiProductosRouter = require('./api/productos');
+const apiUsuariosRouter = require('./api/usuarios');
 
 
-<<<<<<< Updated upstream
-router.use( '/usuarios', apiUsuariosRouter );
-router.use( '/productos', apiProductosRouter );
-=======
 router.use('/productos', apiProductosRouter);
 router.use('/usuarios', apiUsuariosRouter);
->>>>>>> Stashed changes
 
 module.exports = router;
